ed91aa59616ea753780c4583bd30521674a47d13
Vince <40652939+PNP-cpp@users.noreply.github.com>
2024-07-11 02:10:32 +0000

    fix: 1

--- app.vue
+++ app.vue
@@ -1,13 +1,41 @@
 <template>
-  <main class="py-20 px-12 text-center">
-    <span text="blue 5xl hover:red" cursor="default">Hello Nuxt 3</span>
-    <br>
-    <div i-carbon-car text-4xl inline-block />
-    <br>
-    <div style=""></div>
-    <button btn>
-      Button
-    </button>
+  <main class="grid " style="grid-template-columns: 10rem  1fr  10rem">
+    <div class="flex gap-1 flex-col ">
+      <div bg-red>
+        s
+      </div>
+      <div bg-red>
+        s
+      </div>
+      <div bg-red>
+        s
+      </div>
+    </div>
+    <div class="flex gap-1 flex-col flex-1">
+      <div bg-blue>
+        s
+      </div>
+      <div bg-blue>
+        s
+      </div>
+      <div bg-blue>
+        s
+      </div>
+    </div>
+    <div class="flex gap-1  flex-col ">
+      <div>
+        <div bg-green truncate class="w100%" >
+        asdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsad
+      </div>
+      </div>
+   
+      <div bg-green>
+        s
+      </div>
+      <div bg-green>
+        s
+      </div>
+    </div>
   </main>
 </template>
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ export default defineNuxtConfig({
   modules: [
     '@unocss/nuxt',
   ],
+
   unocss: {
     attributify: true,
     icons: true,
@@ -10,4 +11,6 @@ export default defineNuxtConfig({
       ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
     ],
   },
+
+  compatibilityDate: '2024-07-10',
 })
